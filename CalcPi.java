5b99596115e48a1827e1a96001f50aa08fe939ec
Asher Salama <asher.salamaruah@post.runi.ac.il>
2025-11-11 13:18:04 +0200

    finished ex3,4

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,15 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int times = Math.abs(Integer.parseInt(args[0]));
+		double pi = Math.PI;
+		double approximation = 0.0;
+		for (int k = 0; k < times; k++) {
+			double sign = Math.pow(-1.0, k);
+			double terms = sign*(1.0 / (k*2+1));
+			approximation += terms;
+		}
+		System.out.println("pi according to Java: " + pi);
+	    System.out.println("pi, approximated: " + approximation*4);
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,17 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String cheering = (args[0]).toUpperCase();
+                int times = Integer.parseInt(args[1]);
+                for (int x = 0; x < cheering.length(); x++) {
+                        char c = cheering.charAt(x);
+                        String article = ("AEFHILMNORSX".indexOf(c) != -1) ? "an" : "a ";
+                        System.out.println("Give me " + article + " " + c + ": " + c + "!");
+                }
+                System.out.println("What does that spell?");
+                
+                for (int i = 0; i < times; i++) {
+                        System.out.println(cheering + "!!!");
+                }
         }
 }
